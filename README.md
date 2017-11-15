ce51ec1bae58f8561dbb22c23ca5cf8655bf7fc7
Adam Elwood <adam.elwood@desy.de>
2017-11-15 10:39:36 +0100

    updated setup script to include pandas

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ Start by turning your root trees into numpy arrays!
 To do this install root_numpy... http://scikit-hep.org/root_numpy/start.html
 
 For convenience, if you have access to cvmfs just use:
+
 ```source lcgenv.sh```
 
--- lcgenv.sh
+++ lcgenv.sh
@@ -1,22 +1,30 @@
 # found package Python-806f9
 # found package sqlite-87521
+# found package pytz-d9478
 # found package numpy-8b1f8
 # found package Python-806f9
+# found package python_dateutil-8cb24
 # found package setuptools-0f487
 # found package Python-806f9
+# found package six-079c0
 # found package ROOT-27428
 # found package numpy-8b1f8
 # found package dcap-cdd28
 # found package Python-806f9
+# found package setuptools-0f487
 # found package Davix-0c041
 # found package Boost-579a0
 # found package Python-806f9
+# found package setuptools-0f487
+# found package numpy-8b1f8
 # found package fftw-0c601
 # found package Qt-0b84e
 # found package oracle-e33b7
 # found package graphviz-a8340
 # found package xrootd-66949
 # found package Python-806f9
+# found package setuptools-0f487
+# found package setuptools-0f487
 # found package gfal-6fc75
 # found package GSL-36ee5
 # found package R-d7fc6
@@ -26,6 +34,9 @@
 # found package mysql-046eb
 # found package CASTOR-9ccc5
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/gcc/4.9.3/x86_64-slc6/setup.sh;
+export PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/pandas/0.16.1/x86_64-slc6-gcc49-opt/bin:$PATH";
+export LD_LIBRARY_PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/pandas/0.16.1/x86_64-slc6-gcc49-opt/lib:$LD_LIBRARY_PATH";
+export PYTHONPATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/pandas/0.16.1/x86_64-slc6-gcc49-opt/lib/python2.7/site-packages:$PYTHONPATH";
 export LD_LIBRARY_PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/root_numpy/4.5.1/x86_64-slc6-gcc49-opt/lib:$LD_LIBRARY_PATH";
 export PYTHONPATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/root_numpy/4.5.1/x86_64-slc6-gcc49-opt/lib/python2.7/site-packages:$PYTHONPATH";
 export PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/Python/2.7.10/x86_64-slc6-gcc49-opt/bin:$PATH";
@@ -39,12 +50,21 @@ export PYTHON_HOME="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/Python/2.7.10/x8
 cd "/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/Python/2.7.10/x86_64-slc6-gcc49-opt"
 export PYTHONHOME="${PYTHON_HOME}"
 cd - 1>/dev/null # from /cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/Python/2.7.10/x86_64-slc6-gcc49-opt
+export LD_LIBRARY_PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/pytz/2015.4/x86_64-slc6-gcc49-opt/lib:$LD_LIBRARY_PATH";
+export PYTHONPATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/pytz/2015.4/x86_64-slc6-gcc49-opt/lib/python2.7/site-packages:$PYTHONPATH";
+export LD_LIBRARY_PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/python_dateutil/2.4.0/x86_64-slc6-gcc49-opt/lib:$LD_LIBRARY_PATH";
+export PYTHONPATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/python_dateutil/2.4.0/x86_64-slc6-gcc49-opt/lib/python2.7/site-packages:$PYTHONPATH";
+export LD_LIBRARY_PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/six/1.9.0/x86_64-slc6-gcc49-opt/lib:$LD_LIBRARY_PATH";
+export PYTHONPATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/six/1.9.0/x86_64-slc6-gcc49-opt/lib/python2.7/site-packages:$PYTHONPATH";
 export PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/numpy/1.11.0/x86_64-slc6-gcc49-opt/bin:$PATH";
 export LD_LIBRARY_PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/numpy/1.11.0/x86_64-slc6-gcc49-opt/lib:$LD_LIBRARY_PATH";
 export PYTHONPATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/numpy/1.11.0/x86_64-slc6-gcc49-opt/lib/python2.7/site-packages:$PYTHONPATH";
 export PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/setuptools/20.1.1/x86_64-slc6-gcc49-opt/bin:$PATH";
 export LD_LIBRARY_PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/setuptools/20.1.1/x86_64-slc6-gcc49-opt/lib:$LD_LIBRARY_PATH";
 export PYTHONPATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/setuptools/20.1.1/x86_64-slc6-gcc49-opt/lib/python2.7/site-packages:$PYTHONPATH";
+export PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/numpy/1.11.0/x86_64-slc6-gcc49-opt/bin:$PATH";
+export LD_LIBRARY_PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/numpy/1.11.0/x86_64-slc6-gcc49-opt/lib:$LD_LIBRARY_PATH";
+export PYTHONPATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/numpy/1.11.0/x86_64-slc6-gcc49-opt/lib/python2.7/site-packages:$PYTHONPATH";
 export PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/ROOT/v6-07-07-aliceml/x86_64-slc6-gcc49-opt/bin:$PATH";
 export LD_LIBRARY_PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/ROOT/v6-07-07-aliceml/x86_64-slc6-gcc49-opt/lib:$LD_LIBRARY_PATH";
 export LD_LIBRARY_PATH="/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan2/ROOT/v6-07-07-aliceml/x86_64-slc6-gcc49-opt/lib/JupyROOT:$LD_LIBRARY_PATH";
