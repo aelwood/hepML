e15dd9baaa3c3f722609b886aec620890bf1f4dc
Adam Elwood <adam.elwood@desy.de>
2017-11-20 14:30:56 +0100

    added a gram matrix calculation

---
+++ linearAlgebraFunctions.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def dotProduct(vec1,vec2):
+    return np.sqrt(vec1[0]*vec2[0]-vec1[1]*vec2[1]-vec1[2]*vec2[2]-vec1[3]*vec2[3])
+
+def gram(e,px,py,pz):
+    '''Take arrays of e,px,py,pz for N different objects and return an
+    NxN gram matrix (dot product of all the possible pairs)'''
+
+    assert len(e)==len(px)
+    assert len(px)==len(py)
+    assert len(py)==len(pz)
+
+    g = np.empty([len(e),len(e)])
+    for i in range(len(e)):
+        for j in range(len(e)):
+            g[i,j] = dotProduct([e[i],px[i],py[i],pz[i]], [e[j],px[j],py[j],pz[j]])
+    
+    print 'e',e
+    print 'px',px
+    print 'py',py
+    print 'pz',pz
+    print g
+
+    exit()
+    pass
--- run.py
+++ run.py
@@ -1,26 +1,40 @@
 import os
+import pandas as pd
 from dfConvert import convertTree
 from pandasPlotting.Plotter import Plotter
+from linearAlgebraFunctions import gram
 
-saveDfs=True
+makeDfs=False
+saveDfs=True #Save the dataframes if they're remade
+
 makePlots=False
-exceptions=['selJetB']
+
+exceptions=[] #variables to miss out when plotting
+
 
 if __name__=='__main__':
 
-    print "Loading DataFrames" 
-    signalFile = '/nfs/dust/cms/group/susy-desy/marco/training_sample_new/top_sample_0.root'
-    bkgdFile = '/nfs/dust/cms/group/susy-desy/marco/training_sample_new/stop_sample_0.root'
+    #Either make the dataframes fresh from the trees or just read them in
+    if makeDfs:
+        print "Making DataFrames"
 
-    signal = convertTree(signalFile,signal=True,passFilePath=True,tlVectors = ['selJet','sel_lep'])
-    bkgd = convertTree(bkgdFile,signal=False,passFilePath=True,tlVectors = ['selJet','sel_lep'])
+        signalFile = '/nfs/dust/cms/group/susy-desy/marco/training_sample_new/top_sample_0.root'
+        bkgdFile = '/nfs/dust/cms/group/susy-desy/marco/training_sample_new/stop_sample_0.root'
 
-    if saveDfs:
-        print 'Saving the dataframes'
-        # Save the dfs?
-        if not os.path.exists('dfs'): os.makedirs('dfs')
-        signal.to_csv('dfs/signal.csv')
-        bkgd.to_csv('dfs/bkgd.csv')
+        signal = convertTree(signalFile,signal=True,passFilePath=True,tlVectors = ['selJet','sel_lep'])
+        bkgd = convertTree(bkgdFile,signal=False,passFilePath=True,tlVectors = ['selJet','sel_lep'])
+
+        if saveDfs:
+            print 'Saving the dataframes'
+            # Save the dfs?
+            if not os.path.exists('dfs'): os.makedirs('dfs')
+            signal.to_pickle('dfs/signal.pkl')
+            bkgd.to_pickle('dfs/bkgd.pkl')
+    else:
+        print "Loading DataFrames" 
+
+        signal = pd.read_pickle('dfs/signal.pkl')
+        bkgd = pd.read_pickle('dfs/signal.pkl')
 
     if makePlots:
         print "Making plots" 
@@ -32,6 +46,17 @@ if __name__=='__main__':
         pass
 
     #Now add the relevant variables to the DFs (make gram matrix)
+
+    #Make a matrix of J+L x J+L where J is the number of jets and L is the number of leptons
+    #Store it as a numpy matrix in the dataframe 
+    
+    #Use function that takes (4x) arrays of objects (for E,px,py,pz) and returns matrix
+
+    print 'm',signal['selJet_m'][0]+signal['sel_lep_m'][0]
+    signal['gram'] = signal.apply(lambda row: gram(row['selJet_e']+row['sel_lep_e'],\
+        row['selJet_px']+row['sel_lep_px'],row['selJet_py']+row['sel_lep_py'],\
+        row['selJet_pz']+row['sel_lep_pz']),axis=1)
+
     pass
 
     #Now carry out machine learning (with some algo specific diagnostics)
