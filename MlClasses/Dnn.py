0996e7373cd2f143d2d23ef06a53bc4b6d880b3b
Adam Elwood <adam.elwood@desy.de>
2017-12-05 16:01:08 +0100

    finishing basic dnn implementation

--- MlClasses/Dnn.py
+++ MlClasses/Dnn.py
@@ -2,6 +2,15 @@ from keras.models import Sequential
 from keras.layers import Dense
 from MlClasses.PerformanceTests import rocCurve
 
+def findLayerSize(layer,refSize):
+
+    if isinstance(layer, float):
+        return int(layer*refSize)
+    elif isinstance(layer, int):
+        return layer
+    else:
+        print 'WARNING: layer must be int or float'
+        return None
 
 class Dnn(object):
 
@@ -9,7 +18,7 @@ class Dnn(object):
         self.data = data
         self.output = output
 
-    def setup(self,hiddenLayers=[]):
+    def setup(self,hiddenLayers=[1.0]):
 
         '''Setup the neural net. Input a list of hiddenlayers
         if you fill float takes as fraction of inputs+outputs
@@ -17,13 +26,44 @@ class Dnn(object):
         E.g.:  hiddenLayers=[0.66,20] 
         has two hidden layers, one with 2/3 of input plus output 
         neurons, second with 20 neurons.
+        All the layers are fully connected (dense)
         ''' 
 
+        inputSize=len(self.data.X_train.columns)
+
+        #Find number of unique outputs
+        outputSize = len(self.data.y_train.iloc[:,0].unique())
+        refSize=inputSize+outputSize
+
         self.model = Sequential()
-        self.model.add(Dense(units=64, activation='relu', 
-            input_dim=len(self.data.X_train.columns)))
-        self.model.compile(loss='categorical_crossentropy',
-            optimizer='sgd',metrics=['accuracy'])
+
+        assert len(hiddenLayers)>=1, 'Need at least one hidden layer'
+
+        #Add the first layer, taking the inputs
+        self.model.add(Dense(units=findLayerSize(hiddenLayers[0],refSize), 
+            activation='relu', input_dim=inputSize))
+
+        #Add the extra hidden layers
+        for layer in hiddenLayers[1:]:
+            self.model.add(Dense(units=findLayerSize(hiddenLayers[0],refSize), 
+                activation='relu'))
+
+        #Add the output layer and choose the type of loss function
+        #Choose the loss function based on whether it's binary or not
+        if outputSize==2: 
+            #It's better to choose a sigmoid function and one output layer for binary
+            # This is a special case of n>2 classification
+            model.add(Dense(1, activation='sigmoid'))
+            loss = 'binary_crossentropy'
+        else: 
+            #Softmax forces the outputs to sum to 1 so the score on each node
+            # can be interpreted as the probability of getting each class
+            model.add(Dense(outputSize, activation='softmax'))
+            loss = 'categorical_crossentropy'
+
+        #After the layers are added compile the model
+        self.model.compile(loss=loss,
+            optimizer='adam',metrics=['accuracy'])
 
     def fit(self):
 
