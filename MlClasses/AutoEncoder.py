1434c5db9ce4aedb077d5084dae5db6611784d82
Adam Elwood <adam.elwood@desy.de>
2018-10-04 17:30:51 +0200

    added diagnostics for the autoencoder

--- MlClasses/AutoEncoder.py
+++ MlClasses/AutoEncoder.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 
 from MlClasses.Dnn import Dnn
 from MlClasses.Config import Config
 from MlFunctions.DnnFunctions import createAutoencoderModel
+from MlClasses.PerformanceTests import rocCurve
+
+from asimovErrors import Z,eZ
 
 class AutoEncoder(Dnn):
 
@@ -66,7 +70,7 @@ class AutoEncoder(Dnn):
         if subDir:
             self.output=oldOutput
 
-    def plotError(self,customPred=False,customData=None):
+    def plotError(self,customPred=False,customData=None,expectedCounts=None):
 
         data = self.data.X_test.as_matrix()
         append=''
@@ -83,18 +87,72 @@ class AutoEncoder(Dnn):
             data = customData
             append='WithCustom'
             preds = self.model.predict(data) 
-            mse2 = ((data - preds) ** 2).mean(axis=1)
-            maxRange=max(max(mse),max(mse2))
-            plt.hist(mse2,bins=70,range=[0,maxRange],label='signal',alpha=0.7)
+            mseCustom = ((data - preds) ** 2).mean(axis=1)
+            maxRange=max(max(mse),max(mseCustom))
+            plt.hist(mseCustom,bins=100,range=[0,maxRange],label='signal',alpha=0.5)
         else:
             maxRange=max(mse)
 
-        plt.hist(mse,bins=70,range=[0,maxRange],label='bkgd',alpha=0.7)
+        plt.hist(mse,bins=100,range=[0,maxRange],label='bkgd',alpha=0.5)
         plt.yscale('log')
         plt.legend()
 
         if not os.path.exists(self.output): os.makedirs(self.output)
         plt.savefig(os.path.join(self.output,'error'+append+'.pdf'))
         plt.clf()
-        
+
+        #Also make a ROC curve to show separation power
+        if customPred:
+            y_pred = np.concatenate((mse,mseCustom))
+            y_true= np.concatenate((np.zeros(len(mse)),np.ones(len(mseCustom))))
+            rocCurve(y_pred,y_true,self.output)
+
+            if expectedCounts is not None:
+                assert len(expectedCounts)==2,'Need exactly two sets of expected counts'
+                #weightDict={0:expectedCounts[0],1:expectedCounts[1]}
+                weight0=np.full(len(mse),expectedCounts[0]/len(mse))
+                weight1=np.full(len(mseCustom),expectedCounts[1]/len(mseCustom))
+                
+                h1=plt.hist(mse,weights=weight0,
+                        bins=5000,range=[0,maxRange],
+                        alpha=0.8,label='background',cumulative=-1)
+                h2=plt.hist(mseCustom,weights=weight1,
+                        bins=5000,range=[0,maxRange],
+                        color='r',alpha=0.8,label='signal',cumulative=-1)
+                plt.yscale('log')
+                plt.ylabel('Cumulative event counts / 0.02')
+                plt.xlabel('Classifier output')
+                plt.legend()
+                plt.savefig(os.path.join(self.output,'cumulativeWeightedDiscriminator.pdf'))
+                plt.clf()
+
+                s=h2[0]
+                b=h1[0]
+
+                plt.plot((h1[1][:-1]+h1[1][1:])/2,s/b)
+                plt.title('sig/bkgd on test set')
+                plt.savefig(os.path.join(self.output,'sigDivBkgdDiscriminator.pdf'))
+                plt.clf()
+
+                plt.plot((h1[1][:-1]+h1[1][1:])/2,s/np.sqrt(s+b))
+                plt.title('sig/sqrt(sig+bkgd) on test set, best is '+str(max(s/np.sqrt(s+b))))
+                plt.savefig(os.path.join(self.output,'sensitivityDiscriminator.pdf'))
+                plt.clf()
+                systematic=0.1
+
+                toPlot = Z(s,b,systematic)
+                plt.plot((h1[1][:-1]+h1[1][1:])/2,toPlot)
+                es = weight1[0]*np.sqrt(s/weight1[0])
+                eb = weight0[0]*np.sqrt(b/weight0[0])
+                error=eZ(s,es,b,eb,systematic)
+                # plt.plot((h1[1][:-1]+h1[1][1:])/2,toPlot-error)
+                # plt.plot((h1[1][:-1]+h1[1][1:])/2,toPlot+error)
+                plt.fill_between((h1[1][:-1]+h1[1][1:])/2,toPlot-error,toPlot+error,linewidth=0,alpha=0.6)
+                maxIndex=np.argmax(toPlot)
+                plt.title('Systematic '+str(systematic)+', s: '+str(round(s[maxIndex],1))+', b:'+str(round(b[maxIndex],1))+', best significance is '+str(round(toPlot[maxIndex],2))+' +/- '+str(round(error[maxIndex],2)))
+                plt.xlabel('Cut on classifier score')
+                plt.ylabel('Asimov estimate of significance')
+                plt.savefig(os.path.join(self.output,'asimovDiscriminatorSyst'+str(systematic).replace('.','p')+'.pdf'))
+                plt.clf()
+            
 
--- autoEncoderScript.py
+++ autoEncoderScript.py
@@ -12,7 +12,7 @@ from MlClasses.MlData import MlData
 from MlClasses.AutoEncoder import AutoEncoder
 
 #===== Define some useful variables =====
-
+makeAnglesRelative=True
 #=====
 
 output='exampleOut' # an output directory (then make it if it doesn't exist)
@@ -43,13 +43,34 @@ expectedSignal=228.195*0.14*lumi
 print 'The keys are:'
 print dfFull.keys()
 
+if makeAnglesRelative:
+    #Make everything relative to MET Phi
+    for k in dfFull.keys():
+        if 'phi' in k:
+            plt.hist(dfFull[dfFull['signal']==0][k].fillna(0),bins=50,range=[-np.pi,np.pi],alpha=0.7,label='bkgd')
+            plt.hist(dfFull[dfFull['signal']==1][k].fillna(0),bins=50,range=[-np.pi,np.pi],alpha=0.7,label='sig')
+            plt.legend()
+            plt.savefig('exampleOut/autoEncoder/'+k+'before.pdf')
+            plt.clf()
+            #print k, df[k].iloc[0:10],df['METPhi'].iloc[0:10],df[k].iloc[0:10]-df['METPhi'].iloc[0:10]
+
+            dfFull[k]=np.mod(dfFull[k]-dfFull['METPhi'],2*np.pi)
+
+            plt.hist(dfFull[dfFull['signal']==0][k].fillna(0),bins=50,range=[0,2*np.pi],alpha=0.7,label='bkgd')
+            plt.hist(dfFull[dfFull['signal']==1][k].fillna(0),bins=50,range=[0,2*np.pi],alpha=0.7,label='sig')
+            plt.legend()
+            plt.savefig('exampleOut/autoEncoder/'+k+'.pdf')
+            plt.clf()
+
+
 dfBkgd = dfFull[dfFull['signal']==0]
 dfSig = dfFull[dfFull['signal']==1]
 
 #Define and select a subset of the variables:
 
-subset=['HT','MET', #energy sums
-        'MT','MT2W', #topological variables
+subset=[#'HT','MET', #energy sums
+        #'MT','MT2W', #topological variables
+        'MET','METPhi',
         'n_jet','n_bjet', #jet and b-tag multiplicities
         'sel_lep_pt0','sel_lep_eta0','sel_lep_phi0', #lepton 4-vector
         'selJet_phi0','selJet_pt0','selJet_eta0','selJet_m0',# lead jet 4-vector
@@ -58,6 +79,9 @@ subset=['HT','MET', #energy sums
         'selJet_phi3','selJet_pt3','selJet_eta3','selJet_m3',
         'selJet_phi4','selJet_pt4','selJet_eta4','selJet_m4',
         'selJet_phi5','selJet_pt5','selJet_eta5','selJet_m5',
+        'selJet_phi6','selJet_pt6','selJet_eta6','selJet_m6',
+        'selJet_phi7','selJet_pt7','selJet_eta7','selJet_m7',
+        'selJet_phi8','selJet_pt8','selJet_eta8','selJet_m8',
         ]
 
 df=dfBkgd[subset]
@@ -101,7 +125,7 @@ dnnC = AutoEncoder(mlDataC,os.path.join(output,'autoEncoder')) #give it the data
 #build a 2 hidden layer model with 50 neurons each layer
 #Note: if the number of neurons is a float it treats it as a proportion of the input
 # loss is binary cross entropy and one sigmoid neuron is used for output
-dnnC.setup(hiddenLayers=[50,30,10,30,50],dropOut=None,l2Regularization=None,loss='mean_squared_error') 
+dnnC.setup(hiddenLayers=[50,20,5,20,50],dropOut=None,l2Regularization=None,loss='mean_squared_error') 
 
 #fit the defined network with the data passed to it
 #define an early stopping if the loss stops decreasing after 2 epochs
@@ -119,7 +143,7 @@ dfSig = dfSig[subset]
 dfSig = dfSig.sample(len(mlDataC.X_test),random_state=41)
 dfSig=dfSig.fillna(0)
 dfSig= mlDataC.scaler.transform(dfSig)
-dnnC.plotError(customPred=True,customData=dfSig)
+dnnC.plotError(customPred=True,customData=dfSig,expectedCounts=[expectedBkgd,expectedSignal])
 
 ## hep specific plots including sensitivity estimates with a flat systematic etc: 
 #print '\nMaking HEP plots'
